8827eb5d8fd4112468a292c6de553f5b1a6ae20d
GlennZhu <julianhaha1@gmail.com>
2015-07-11 22:04:42 -0700

    working

--- src/main/java/controllers/RescueController.java
+++ src/main/java/controllers/RescueController.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import models.RescueStatus;
@@ -12,19 +13,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RescueController {
+	private String rootUrl = "https://88d800c0.ngrok.io";
 	private RescueStatus rescueStatus = new RescueStatus();
+	private long id = 0;
 	
-	@RequestMapping("/registerRescue")
-	public ResponseEntity<Map<Long, Boolean>> registerRescue(@RequestParam(value="rescueId", defaultValue = "") long rescueId) {
-		boolean result = rescueStatus.registerRescue(rescueId);
-		if (!result) {
-			return new ResponseEntity<Map<Long,Boolean>>(HttpStatus.INTERNAL_SERVER_ERROR);
-		} else {
-			return new ResponseEntity<Map<Long, Boolean>>(rescueStatus.getStatus(), HttpStatus.OK);			
-		}
-	}
+//	@RequestMapping("/registerRescue")
+//	public ResponseEntity<Map<Long, Boolean>> registerRescue(@RequestParam(value="rescueId", defaultValue = "") long rescueId) {
+//		boolean result = rescueStatus.registerRescue(rescueId);
+//		if (!result) {
+//			return new ResponseEntity<Map<Long,Boolean>>(HttpStatus.INTERNAL_SERVER_ERROR);
+//		} else {
+//			return new ResponseEntity<Map<Long, Boolean>>(rescueStatus.getStatus(), HttpStatus.OK);			
+//		}
+//	}
 	
-	@RequestMapping("/rescue")
+	@RequestMapping("/yes")
 	public ResponseEntity<Map<Long, Boolean>> rescue(@RequestParam(value = "rescueId", defaultValue = "") long rescueId) {
 		boolean result = rescueStatus.rescue(rescueId);
 		if (!result) {
@@ -34,14 +37,28 @@ public class RescueController {
 		}
 	}
 	
+	@RequestMapping("/no")
+	public void deny() {} // No-op
+	
 	@RequestMapping("/isRescued")
-	public ResponseEntity<Boolean> isRescued(long rescueId) {
+	public ResponseEntity<Boolean> isRescued(@RequestParam(value = "rescueId", defaultValue = "") long rescueId) {
 		Boolean result = rescueStatus.isRescued(rescueId);
 		if (result == null) {
 			return new ResponseEntity<Boolean>(HttpStatus.INTERNAL_SERVER_ERROR); 
 		} else {
 			return new ResponseEntity<Boolean>(result, HttpStatus.OK);	
 		}
+	}
+	
+	@RequestMapping("/getLinks")
+	public ResponseEntity<Map<String, String>> getLinks() {
+		Map<String, String> message = new HashMap<>();
+		long thisId = id++;
+		rescueStatus.registerRescue(thisId);
+		message.put("id", "" + thisId);
+		message.put("yesLink", rootUrl + "/yes?rescueId=" + thisId);
+		message.put("DenyUrl", rootUrl + "/no");
+		return new ResponseEntity<Map<String, String>>(message, HttpStatus.OK);
 	}
 	
 	@RequestMapping("/clear")
